c66b586024c8ebd9e86205d5f98e8692f67a8b31
Geraint White <geraintwhite@gmail.com>
2014-08-28 18:26:08 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var http = require('http'),
+    url = require('url');
+
+var router = function(req, res) {
+    path = url.parse(req.url).pathname;
+    console.log('Request for', path);
+    switch(path) {
+        case '/twitter':
+            require('twitter')(req, res);
+            break;
+        case '/sysinfo':
+            require('sysinfo')(req, res);
+            break;
+        case '/wakeup':
+            require('wakeup')(req, res);
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            console.log('404');
+            res.write('404 - Not Found');
+            res.end();
+    }
+};
+
+var port = 6001;
+var app = http.createServer(router);
+
+app.listen(port);
+console.log('Server running on port', port);
