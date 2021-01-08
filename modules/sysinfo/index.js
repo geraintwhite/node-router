f4436821047df0e1c926df9583f5e876e0d71f06
Geraint White <geraint.white@bt.com>
2021-01-08 10:26:55 +0000

    Add speedtest to sysinfo

--- modules/sysinfo/index.js
+++ modules/sysinfo/index.js
@@ -4,29 +4,43 @@ var exec = require('child_process').exec,
 
 var cpu = [],
     mem = [],
-    temp = [];
+    temp = [],
+    upload = [],
+    download = [];
 
 var delay = 10 * 60 * 1000;
 var datacap = 7 * 24 * 6;
+var bytes = 8 / 1000 / 1000;
+
+function convertBytes(value) {
+    return Math.round(value * bytes * 10) / 10;
+}
 
 var graphs = function() {
     async.parallel({
         cpu: function(cb) { exec("top -bn1|awk 'NR>7{s+=$9}END{print s}'", cb); },
         mem: function(cb) { exec("free -h|grep '\\-/+ buffers/cache'|awk '{print$3}'|sed 's/.$//'", cb); },
-        temp: function(cb) { exec("vcgencmd measure_temp|sed \"s/[a-z]*=\\(.*\\)'.*/\\1/g\"", cb); }
+        temp: function(cb) { exec("vcgencmd measure_temp|sed \"s/[a-z]*=\\(.*\\)'.*/\\1/g\"", cb); },
+        speed: function(cb) { exec("speedtest -f json", cb); }
     },
     function(error, stdout, stderr) {
         time = Date.now();
         if (error) {
             console.log(new Date(time), stderr);
         } else {
+            console.log(stdout, stderr);
+
             cpu.push([time, parseInt(stdout.cpu[0].slice(0, -1))]);
             mem.push([time, parseInt(stdout.mem[0].slice(0, -1))]);
             temp.push([time, parseInt(stdout.temp[0].slice(0, -1))]);
+            upload.push([time, convertBytes(JSON.parse(stdout.speed[0]).upload.bandwidth)]);
+            download.push([time, convertBytes(JSON.parse(stdout.speed[0]).download.bandwidth)]);
 
             if (cpu.length > datacap) cpu.shift();
             if (mem.length > datacap) mem.shift();
             if (temp.length > datacap) temp.shift();
+            if (upload.length > datacap) upload.shift();
+            if (download.length > datacap) download.shift();
         }
         setTimeout(graphs, delay);
     });
@@ -38,7 +52,7 @@ module.exports = function(req, res) {
 
     if (url.parse(req.url, true).query.graph !== undefined) {
         res.writeHead(200, {'Content-Type': 'application/json'});
-        res.write(JSON.stringify({ cpu: cpu, mem: mem, temp: temp }));
+        res.write(JSON.stringify({ cpu, mem, temp, upload, download }));
         res.end();
     } else if (url.parse(req.url, true).query.pebble != undefined) {
         res.writeHead(200, {'Content-Type': 'application/json'});
